f5faa900aa04c7b168dcf829ae541a8495b104ca
Cory Louis Vertentes <cory@Corys-MacBook-Pro.local>
2020-03-25 17:29:20 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,56 @@
-console.log("Up and running!");
+/*console.log("Up and running!");
 let cardOne = "queen"
 let cardTwo = "queen"
 let cardThree = "king"
 let cardFour = "king"
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardThree);*/
+
+let cards = ["queen","queen","king","king"];
+let cardsInPlay = [];
+let cardId;
+cardsInPlay.push(cards[cardId]);
+
+function checkForMatch(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+};
+
+function flipCard(cardId){
+	checkForMatch();
+	console.log("User flipped " + cards[cardId]);
+	
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+/*cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+cardTwo = cards[1];
+cardsInPlay.push(cardTwo)
+
+if (cardsInPlay === cardOne)
+{
+console.log('User flipped queen.');
+}
+//console.log("User flipped queen")
+
+
+
+
+function hotOrNot(temp){
+	if (temp >= 70){
+	return "Hot!";}
+	else{
+		return "Not hot!";
+	}
+	};
+*/
+ 
