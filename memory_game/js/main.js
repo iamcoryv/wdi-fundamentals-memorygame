bd481dd09d5521ba98376b97f30cbb4034a38c46
Cory Louis Vertentes <cory@Corys-MacBook-Pro.local>
2020-03-27 19:01:40 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,51 +6,68 @@ let cardFour = "king"
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);*/
 
-let cards = ["queen","queen","king","king"];
-let cardsInPlay = [];
-let cardId;
-cardsInPlay.push(cards[cardId]);
+//let cards = ["queen","queen","king","king"];
 
-function checkForMatch(){
-if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again.");
-}
-};
-
-function flipCard(cardId){
-	checkForMatch();
-	console.log("User flipped " + cards[cardId]);
-	
-};
-
-flipCard(0);
-flipCard(2);
-
-
-
-/*cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-cardTwo = cards[1];
-cardsInPlay.push(cardTwo)
-
-if (cardsInPlay === cardOne)
+var cards = [
 {
-console.log('User flipped queen.');
+	rank: "queen",
+	suit: "hearts",
+	cardImage:"images/queen-of-hearts.png", 
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage:"images/queen-of-diamonds.png", 
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage:"images/king-of-hearts.png", 
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage:"images/king-of-diamonds.png", 
 }
-//console.log("User flipped queen")
+];
+//let cardElement;
 
 
 
 
-function hotOrNot(temp){
-	if (temp >= 70){
-	return "Hot!";}
-	else{
-		return "Not hot!";
-	}
-	};
-*/
- 
+var cardsInPlay = [];
+//let cardId;
+//cardsInPlay.push(cards[cardId].rank);
+
+function checkForMatch() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+ };
+};
+
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2){
+	checkForMatch();
+ };
+};
+	
+
+console.log(cardsInPlay);
+
+function createBoard() {
+	const cardTable = document.getElementById('game-board');
+	for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src','images/back.png');
+    cardElement.setAttribute('data-id',i);
+    cardElement.addEventListener('click',flipCard);
+    cardTable.appendChild(cardElement);
+ };
+};
+
+createBoard();	
